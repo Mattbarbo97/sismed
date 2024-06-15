2189c2661aabaacb1532a4415a2ab9a7ec53c6b8
Mattbarbo97 <138821344+Mattbarbo97@users.noreply.github.com>
2024-06-15 02:07:38 -0300

    Update GestaoHorario.js

--- src/components/menu/agendamentos/GestaoHorario.js
+++ src/components/menu/agendamentos/GestaoHorario.js
@@ -16,7 +16,7 @@ import {
   CardContent,
   FormControlLabel,
   Switch
-} from '@mui/material';
+} from '@mui/material'; // Corrigido aqui
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import format from 'date-fns/format';
 import parse from 'date-fns/parse';

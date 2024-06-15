b14fdca9d616d3cb52fdde7273728ab4f51f30a9
Mattbarbo97 <138821344+Mattbarbo97@users.noreply.github.com>
2024-06-15 01:17:13 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFunctions } from "firebase/functions";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHDLett-AYkEt3ngDZCBPZZbmX8zjdAIU",
+  authDomain: "unna-6c98e.firebaseapp.com",
+  projectId: "unna-6c98e",
+  storageBucket: "unna-6c98e.appspot.com",
+  messagingSenderId: "212998177734",
+  appId: "1:212998177734:web:4a4383388ba4b94ee2a8c7",
+  measurementId: "G-LHHFSH5E5C"
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
+const functions = getFunctions(app);
+
+export { auth, firestore, storage, db, functions };
